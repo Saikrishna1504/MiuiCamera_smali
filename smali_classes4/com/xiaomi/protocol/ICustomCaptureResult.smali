aosp-trees/MiuiCamera-smali

ICustomCaptureResult: Add readout timestamp
commit [1] added two arguments to the CaptureResultExtras constructor.
Patch MiuiCamera to provide these arguments

https://github.com/LineageOS/android_frameworks_base/commit/91e7522cb28d2b1e2e12adc8726db8190d711f2e

smali_classes4/com/xiaomi/protocol/ICustomCaptureResult.smali
@@ -412,7 +412,7 @@
 .end method
 
 .method public static toTotalCaptureResult(Lcom/xiaomi/protocol/ICustomCaptureResult;IZ)Landroid/hardware/camera2/TotalCaptureResult;
-    .locals 23
+    .locals 27
     .annotation system Ldalvik/annotation/MethodParameters;
         accessFlags = {
             0x0,
@@ -509,7 +509,7 @@
 
     if-lt v1, v8, :cond_0
 
-    const/16 v1, 0xb
+    const/16 v1, 0xd
 
     :try_start_1
     new-array v8, v1, [Ljava/lang/Class;
@@ -546,6 +546,18 @@
     const/16 v21, 0xa
 
     aput-object v20, v8, v21
+
+    sget-object v20, Ljava/lang/Boolean;->TYPE:Ljava/lang/Class;
+
+    const/16 v23, 0xb
+
+    aput-object v20, v8, v23
+
+    sget-object v20, Ljava/lang/Long;->TYPE:Ljava/lang/Class;
+
+    const/16 v24, 0xc
+
+    aput-object v20, v8, v24
 
     .line 7
     invoke-virtual {v7, v8}, Ljava/lang/Class;->getDeclaredConstructor([Ljava/lang/Class;)Ljava/lang/reflect/Constructor;
@@ -623,6 +635,22 @@
     move-result-object v19
 
     aput-object v19, v1, v21
+
+    const/16 v25, 0x0
+
+    invoke-static/range {v25 .. v25}, Ljava/lang/Boolean;->valueOf(Z)Ljava/lang/Boolean;
+
+    move-result-object v20
+
+    aput-object v20, v1, v23
+
+    const/16 v26, 0x0
+
+    invoke-static/range {v26 .. v26}, Ljava/lang/Integer;->valueOf(I)Ljava/lang/Integer;
+
+    move-result-object v20
+
+    aput-object v20, v1, v24
 
     .line 10
     invoke-virtual {v8, v1}, Ljava/lang/reflect/Constructor;->newInstance([Ljava/lang/Object;)Ljava/lang/Object;

Saikrishna1504/MiuiCamera_smali

Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

smali_classes4/com/xiaomi/protocol/ICustomCaptureResult.smali
@@ -322,7 +322,7 @@
 .end method
 
 .method public static toTotalCaptureResult(Lcom/xiaomi/protocol/ICustomCaptureResult;IZ)Landroid/hardware/camera2/TotalCaptureResult;
-    .locals 22
+    .locals 26
 
     const-string v0, "|"
 
@@ -406,7 +406,7 @@
 
     if-lt v0, v7, :cond_0
 
-    const/16 v0, 0xb
+    const/16 v0, 0xd
 
     :try_start_1
     new-array v7, v0, [Ljava/lang/Class;
@@ -441,6 +441,18 @@
     const/16 v20, 0xa
 
     aput-object v19, v7, v20
+
+    sget-object v19, Ljava/lang/Boolean;->TYPE:Ljava/lang/Class;
+
+    const/16 v22, 0xb
+
+    aput-object v19, v6, v22
+
+    sget-object v19, Ljava/lang/Long;->TYPE:Ljava/lang/Class;
+
+    const/16 v23, 0xc
+
+    aput-object v19, v6, v23
 
     .line 8
     invoke-virtual {v6, v7}, Ljava/lang/Class;->getDeclaredConstructor([Ljava/lang/Class;)Ljava/lang/reflect/Constructor;
@@ -516,6 +528,22 @@
     move-result-object v18
 
     aput-object v18, v0, v20
+
+    const/16 v24, 0x0
+
+    invoke-static/range {v24 .. v24}, Ljava/lang/Boolean;->valueOf(Z)Ljava/lang/Boolean;
+
+    move-result-object v19
+
+    aput-object v19, v0, v22
+
+    const/16 v25, 0x0
+
+    invoke-static/range {v25 .. v25}, Ljava/lang/Integer;->valueOf(I)Ljava/lang/Integer;
+
+    move-result-object v19
+
+    aput-object v19, v0, v23
 
     .line 11
     invoke-virtual {v7, v0}, Ljava/lang/reflect/Constructor;->newInstance([Ljava/lang/Object;)Ljava/lang/Object;